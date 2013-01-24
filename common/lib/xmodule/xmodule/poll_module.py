6ff7aa62adc70e8add69aa53cfe890d2aacd2674
Alexander Kryklia <kryklia@gmail.com>
2013-01-24 13:47:42 +0200

    poll logics

--- common/lib/xmodule/xmodule/poll_module.py
+++ common/lib/xmodule/xmodule/poll_module.py
@@ -28,6 +28,7 @@ import json
 import logging
 from lxml import etree
 import xmltodict
+from lxml import html
 
 from xmodule.mako_module import MakoModuleDescriptor
 from xmodule.xml_module import XmlDescriptor
@@ -43,16 +44,11 @@ log = logging.getLogger(__name__)
 class PollModule(XModule):
     ''' Poll Module
     '''
-
     js = {
-       'coffee': [resource_string(__name__, 'js/src/javascript_loader.coffee'),
-                  # resource_string(__name__, 'js/src/poll/poll_main.coffee')
-                  ],
+      'coffee': [resource_string(__name__, 'js/src/javascript_loader.coffee')],
       'js': [resource_string(__name__, 'js/src/poll/poll.js'),
-            resource_string(__name__, 'js/src/poll/poll_main.js'),
-
-      ]
-    }
+             resource_string(__name__, 'js/src/poll/poll_main.js')]
+         }
     css = {'scss': [resource_string(__name__, 'css/poll/display.scss')]}
     js_module_name = "Poll"
 
@@ -65,6 +61,9 @@ class PollModule(XModule):
         XModule.__init__(self, system, location, definition, descriptor,
                          instance_state, shared_state, **kwargs)
         self.system = system
+        self.html_id = self.location.html_id()
+        self.html_class = self.location.category
+        self.poll_units_list = []
 
         if instance_state is not None:
             self.state = json.loads(instance_state)
@@ -90,135 +89,62 @@ class PollModule(XModule):
                 return json.dumps({'success': False})
         # raise NotFoundError('Unexpected dispatch type')
 
-    # def handle_ajax(self, dispatch, get, system):
-    #     """
-    #     This is called by courseware.module_render, to handle an AJAX call.
-    #     "get" is request.POST.
-
-    #     Returns a json dictionary:
-    #     { 'progress_changed' : True/False,
-    #     'progress': 'none'/'in_progress'/'done',
-    #     <other request-specific values here > }
-    #     """
-
-    #     handlers = {
-    #         'save_answer': self.save_answer,
-    #         'save_assessment': self.save_assessment,
-    #         'save_post_assessment': self.save_hint,
-    #     }
-
-    #     if dispatch not in handlers:
-    #         return 'Error'
-
-    #     before = self.get_progress()
-    #     d = handlers[dispatch](get, system)
-    #     after = self.get_progress()
-    #     d.update({
-    #         'progress_changed': after != before,
-    #         'progress_status': Progress.to_js_status_str(after),
-    #     })
-    #     return json.dumps(d, cls=ComplexEncoder)
-
-
-    # def answer_first_question(self, get, system):
-    #     """
-    #     After the answer is submitted, show the plot.
-
-    #     Args:
-    #         get: the GET dictionary passed to the ajax request.  Should contain
-    #             a key 'student_answer'
-
-    #     Returns:
-    #         Dictionary with keys 'success' and either 'error' (if not success),
-    #         or 'rubric_html' (if success).
-    #     """
-    #     # Check to see if attempts are less than max
-    #     if self.attempts > self.max_attempts:
-    #         # If too many attempts, prevent student from saving answer and
-    #         # seeing rubric.  In normal use, students shouldn't see this because
-    #         # they won't see the reset button once they're out of attempts.
-    #         return {
-    #             'success': False,
-    #             'error': 'Too many attempts.'
-    #         }
-
-    #     if self.state != self.INITIAL:
-    #         return self.out_of_sync_error(get)
-
-    #     # add new history element with answer and empty score and hint.
-    #     self.new_history_entry(get['student_answer'])
-    #     self.change_state(self.ASSESSING)
-
-    #     return {
-    #         'success': True,
-    #         'plot_html': self.get_plot_and_second_answer(system)
-    #     }
-
-    # def get_plot_and_second_answer(self, system):
-    #     """
-    #     Return the appropriate version of the rubric, based on the state.
-    #     """
-    #     if self.state == self.INITIAL:
-    #         return ''
-
-    #     rubric_html  = CombinedOpenEndedRubric.render_rubric(self.rubric)
-
-    #     # we'll render it
-    #     context = {'rubric': rubric_html,
-    #                'max_score': self._max_score,
-    #     }
-
-    #     if self.state == self.ASSESSING:
-    #         context['read_only'] = False
-    #     elif self.state in (self.POST_ASSESSMENT, self.DONE):
-    #         context['read_only'] = True
-    #     else:
-    #         raise ValueError("Illegal state '%r'" % self.state)
-
-    #     return system.render_template('self_assessment_rubric.html', context)
-
     def get_html(self):
         """ Renders parameters to template. """
-        #set context variables and render template
-        # if self.state != self.INITIAL:
-        #     latest = self.latest_answer()
-        #     previous_answer = latest if latest is not None else ''
-        # else:
-        #     previous_answer = ''
 
-        # these 3 will be used in class methods
-        self.html_id = self.location.html_id()
-        self.html_class = self.location.category
-        self.configuration_json = self.build_configuration_json()
         params = {
                   'element_html': self.definition['render'],
                   'element_id': self.html_id,
                   'element_class': self.html_class,
-                  'configuration_json': self.configuration_json,
-                 'ajax_url': self.system.ajax_url,
-            'state': 1,
-            'allow_reset': True,
-            'child_type': 'poll'
+                  'ajax_url': self.system.ajax_url,
+                  'poll_units': self.parse_sequence(self.definition['sequence']),
                   }
-        # import ipdb; ipdb.set_trace()
         self.content = self.system.render_template(
                         'poll.html', params)
-        # import ipdb; ipdb.set_trace()
         return self.content
 
-    def build_configuration_json(self):
-        """Creates json element from xml element (with aim to transfer later
-         directly to javascript via hidden field in template). Steps:
+    def get_poll_unit_html(self, i, question):
+        """ """
+        return """
+<div id="poll_unit_{poll_number}" class="polls"> {question}
+  <div class="vote_and_submit">
+    <div id="vote_block-1" class="vote">
+      <ul>
+        <li>
+          <input type="radio" id="vote_{poll_number}" name="vote_{poll_number}" value="Yes" />
+            <label for="vote_{poll_number}">Yes</label>
+          </li>
+          <li>
+            <input type="radio" id="vote_{poll_number}" name="vote_{poll_number}" value="No" />
+            <label for="vote_{poll_number}">No</label>
+          </li>
+        </ul>
+      </div>
 
-            1. Convert xml tree to python dict.
+    <div class="vote_submit_button">
+        <input type="button" value="Cast Your vote" class=".submit-button"/>
+    </div>
+  </div>
 
-            2. Dump dict to json.
+  <div class="graph_answer"></div>
+</div>""".format(poll_number=i, question=question)
 
-        """
-        # <root> added for interface compatibility with xmltodict.parse
-        # class added for javascript's part purposes
-        return json.dumps(xmltodict.parse('<root class="' + self.html_class +
-                '">' + self.definition['configuration'] + '</root>'))
+    def parse_sequence(self, html_string):
+        """    substitute sequence     """
+        xml = html.fromstring(html_string)
+        poll_units = xml.xpath('//unit')
+        for i, poll_unit in enumerate(poll_units):
+            # import ipdb; ipdb.set_trace()
+            poll_unit_question = stringify_children(poll_unit.xpath('//question')[0])
+            poll_unit_html = html.fromstring(self.get_poll_unit_html(i,
+                                             poll_unit_question))
+
+            poll_unit_controls = (poll_unit.get('plot', "no"),
+                             poll_unit.get('next_yes', "end"),
+                             poll_unit.get('next_no', "end"))
+            self.poll_units_list.append((poll_unit_html,
+                                   poll_unit_controls))
+        import ipdb; ipdb.set_trace()
 
 
 class PollDescriptor(MakoModuleDescriptor, XmlDescriptor):
@@ -238,10 +164,10 @@ class PollDescriptor(MakoModuleDescriptor, XmlDescriptor):
             dict
         """
         # check for presense of required tags in xml
-        expected_children_level_0 = ['render', 'configuration']
+        expected_children_level_0 = ['render', 'sequence']
         for child in expected_children_level_0:
             if len(xml_object.xpath(child)) != 1:
-                raise ValueError("Graphical Slider Tool definition must include \
+                raise ValueError("Poll definition must include \
                     exactly one '{0}' tag".format(child))
 
         def parse(k):
@@ -249,7 +175,7 @@ class PollDescriptor(MakoModuleDescriptor, XmlDescriptor):
             return stringify_children(xml_object.xpath(k)[0])
         return {
                     'render': parse('render'),
-                    'configuration': parse('configuration')
+                    'sequence': parse('sequence')
                 }
 
     def definition_to_xml(self, resource_fs):
@@ -261,7 +187,7 @@ class PollDescriptor(MakoModuleDescriptor, XmlDescriptor):
             child_node = etree.fromstring(child_str)
             xml_object.append(child_node)
 
-        for child in ['render', 'configuration']:
+        for child in ['render', 'sequence']:
             add_child(child)
 
         return xml_object
