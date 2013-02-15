e0526430a07a27e3819aabfd7a68678349508b88
John Jarvis <jarv@edx.org>
2013-02-15 00:00:37 +0000

    Adding logic to handle contact form submissions

--- common/djangoapps/feedback/views.py
+++ common/djangoapps/feedback/views.py
@@ -7,36 +7,38 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
-
+from django.core.exceptions import ValidationError
 import json
 
 
-#@require_POST
-@csrf_exempt
+@require_POST
+@csrf_protect
 def submit_feedback(request):
 
     email = request.POST.get('email', None)
 
     if email is None and user.is_authenticated:
         email = request.user.email
+    try:
+        validate_email(email)
+    except ValidationError:
+        return HttpResponse(json.dumps({'error': 'Invalid email'}),
+                                content_type="application/json")
 
-    category = request.POST.get('category', None)
-    msg = request.POST.get('msg', None)
+    category = request.POST.get('category', '')
+    description = request.POST.get('description', '')
 
     from_email = 'info@edx.org'
     recipient_list = ['jarv@edx.org']
 
-    if category not in ['general', 'issue', 'account']:
-        return HttpResponse(json.dumps({'error': 'Invalid category'}))
-    elif msg is None or len(msg) > 500:
-        return HttpResponse(json.dumps({'error': 'Message invalid'}))
+    if category not in ['general', 'issue', 'account', 'other']:
+        return HttpResponse(json.dumps({'error': 'Invalid category'}),
+                                content_type="application/json")
+    elif not description or len(description) > 500:
+        return HttpResponse(json.dumps({'error': 'Message invalid'}),
+                                content_type="application/json")
 
-    msg = strip_tags(msg)
-
-    try:
-        validate_email(email)
-    except ValidateError:
-        return HttpResponse(json.dump({'error': 'Invalid email'}))
+    description = strip_tags(description)
 
     user_info = """
     User = {user}
@@ -47,20 +49,19 @@ def submit_feedback(request):
         ip=request.META['REMOTE_ADDR'],
         user_agent = request.META['HTTP_USER_AGENT'])
 
-    msg = msg + user_info
+    description = description + user_info
     subject = '[{0}] - User feedback'.format(category)
 
 
     try:
         send_mail(subject,
-                  msg, from_email, recipient_list,
+                  description, from_email, recipient_list,
                   fail_silently=False, auth_user=None,
                   auth_password=None, connection=None)
+        return HttpResponse(json.dumps({'success': 'Submitted'}),
+                                content_type="application/json")
     except:
-        log.exception(sys.exc_info())
         return HttpResponse(
             {'error': 'Could not send, please try again later'})
-
-    return HttpResponse(json.dumps({'msg': 'Submitted'}))
 
 
