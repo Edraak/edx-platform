e24590860af44cc8a1f32a952598976eb3ee355e
ichuang <ichuang@mit.edu>
2013-02-23 04:50:35 +0000

    add searching to admin for courseware model

--- lms/djangoapps/courseware/admin.py
+++ lms/djangoapps/courseware/admin.py
@@ -6,8 +6,13 @@ from courseware.models import *
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-admin.site.register(StudentModule)
+class StudentModuleAdmin(admin.ModelAdmin):
+    search_fields = ['student__username', 'module_state_key', 'course_id', 'module_type']
+    date_hierarchy = 'created'
+
+admin.site.register(StudentModule, StudentModuleAdmin)
 
 admin.site.register(OfflineComputedGrade)
 
 admin.site.register(OfflineComputedGradeLog)
+
