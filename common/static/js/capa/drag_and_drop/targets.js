b9dd769af0bd7b7e18de80d02a4c75972c52511a
Anto Stupak <s2pak.anton@gmail.com>
2013-04-15 15:32:24 +0300

    NOT CLEAR: make lines in DnD prettier. Fixed bug in non-grid DnD

--- common/static/js/capa/drag_and_drop/targets.js
+++ common/static/js/capa/drag_and_drop/targets.js
@@ -173,6 +173,7 @@ define(['logme'], function (logme) {
                         'h': height,
                         'x': obj.x + i * width,
                         'y': obj.y + j * height,
+                        'isGrid': true,
                         'isFirstCol': (i === 0) ? true : false,
                         'isFirstRow': (j === 0) ? true : false
                     }
@@ -197,12 +198,16 @@ define(['logme'], function (logme) {
 
         borderCss = '';
         if (state.config.targetOutline === true) {
-            borderCss = 'border-width: ' +
+            if (obj.isGrid) {
+                borderCss = 'border-width: ' +
                         ((obj.isFirstRow) ? '1px ' : '0 ')  +
                         '1px 1px ' + 
                         ((obj.isFirstCol) ? '1px; ' : '0; ')  +
                         'border-style: dashed; ' +
                         'border-color: #ccc;';
+            } else{
+                borderCss = 'border: 1px dashed grey;'
+            }
         }
 
         targetEl = $(
