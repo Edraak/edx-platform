ce3008a5090711f89e7b2a1767c68d6ffe37b240
Chris Dodge <cdodge@edx.org>
2013-04-22 16:26:42 -0400

    get initial 'course' api method working

---
+++ common/djangoapps/courseware_api/tastypie/course.py
@@ -0,0 +1,76 @@
+import time
+from tastypie import fields
+from tastypie.authentication import Authentication
+from tastypie.authorization import Authorization
+from tastypie.exceptions import NotFound
+from tastypie.resources import Resource
+from django.contrib.auth.models import User
+from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.exceptions import ItemNotFoundError
+from xmodule.contentstore.content import StaticContent
+from xmodule.course_module import CourseDescriptor
+
+from xblock.core import Scope
+
+from student.models import CourseEnrollment
+
+
+class CourseObject(object):
+    def __init__(self):
+        self.id = ''
+        self.blocks = {}
+
+class CourseResource(Resource):
+    id = fields.CharField(attribute='id')
+    blocks = fields.CharField(attribute='blocks')
+
+    class Meta:
+        resource_name = 'course'
+        object_class = CourseObject
+        authentication = Authentication()
+        authorization = Authorization()
+        include_resource_uri = False
+
+    def obj_get(self, request=None, **kwargs):
+        # return RiakObject(initial={'name': 'bar'})
+        id = kwargs['pk']
+
+        store = modulestore()
+        course_loc = CourseDescriptor.id_to_location(id)
+        try:
+            course = store.get_instance(id, course_loc, depth=4)
+        except:
+            raise NotFound
+
+        # @TODO: Check for authorization
+
+        result = CourseObject()
+        # crawl through the course and create the course outline document
+        def add_xblock_summary(module, result):
+            metadata = {}
+            for field in module.fields + module.lms.fields:
+                # Only save metadata that wasn't inherited
+                if field.scope != Scope.settings:
+                    continue
+
+                try:
+                    metadata[field.name] = module._model_data[field.name]
+                except KeyError:
+                    # Ignore any missing keys in _model_data
+                    pass
+
+            summary = {
+                'category': module.location.category,
+                'metadata': metadata,
+                'children': getattr(module, 'children', []),
+                'definition': module.location.url()
+            }
+            result.blocks[module.location.url()] = summary
+            for child in module.get_children():
+                add_xblock_summary(child, result)
+
+        add_xblock_summary(course, result)
+
+        result.id = id
+        return result
+
--- common/djangoapps/courseware_api/urls.py
+++ common/djangoapps/courseware_api/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import *
-from tastypie.api import ProfileResource
+from tastypie.profile import ProfileResource
+from tastypie.course import CourseResource
 
 profile_resource = ProfileResource()
+course_resource = CourseResource()
 
 urlpatterns = patterns('',
     (r'^', include(profile_resource.urls)),
+    (r'^', include(course_resource.urls)),
 )
+
