b99e803adfed53ac3b87bdfb1215bd28a2ce8efa
Alexander Kryklia <kryklia@gmail.com>
2013-05-22 20:13:14 +0300

    fixes draganddrop locally

---
+++ common/lib/draganddrop/draganddrop/__init__.py
@@ -0,0 +1,1 @@
+from .draganddrop import grade
---
+++ common/lib/draganddrop/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="draganddrop",
+    version="0.1.1",
+    packages=["draganddrop"],
+    install_requires=[
+    ],
+)
