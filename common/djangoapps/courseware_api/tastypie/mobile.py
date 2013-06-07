7560a8bcb32213d2da253a2ec4e5128e5e1756e0
Chris Dodge <cdodge@edx.org>
2013-06-07 11:39:58 -0400

    clean up some stuff in mobile api

--- common/djangoapps/courseware_api/tastypie/mobile.py
+++ common/djangoapps/courseware_api/tastypie/mobile.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from tastypie.http import HttpUnauthorized, HttpForbidden
+from tastypie.http import HttpUnauthorized, HttpForbidden, HttpNotFound
+from tastypie.exceptions import ImmediateHttpResponse
 from django.conf.urls import url
 from tastypie.utils import trailing_slash
 from tastypie.resources import Resource
@@ -32,7 +33,7 @@ class MobileResource(Resource):
             url(r"^(?P<resource_name>%s)/register%s$" %
                 (self._meta.resource_name, trailing_slash()),
                 self.wrap_view('register'), name="mobile_register"),
-            url(r'^(?P<resource_name>%s)/(?P<device_token>[\w\d:/_.-]+)/poll%s$' %
+            url(r'^(?P<resource_name>%s)/(?P<mobile_token>[\w\d:/_.-]+)/poll%s$' %
                 (self._meta.resource_name, trailing_slash()),
                 self.wrap_view('poll'), name='mobile_poll'),
         ]
@@ -47,9 +48,9 @@ class MobileResource(Resource):
         user_token = data.get('user_token', '')
         user = get_user_from_token(user_token)
 
-        device_token = data.get('device_token', '')
+        mobile_token = data.get('mobile_token', '')
 
-        USER_TO_MOBILE_TOKEN_MAP[user_id] = device_token
+        USER_TO_MOBILE_TOKEN_MAP[user_id] = mobile_token
 
     def poll(self, request, **kwargs):
         self.method_check(request, allowed=['get'])
@@ -60,21 +61,21 @@ class MobileResource(Resource):
         if not user_id:
             raise HttpForbidden
 
-        device_token = kwargs['device_token']
+        mobile_token = kwargs['mobile_token']
 
-        if device_token in MOBILE_NOTIFICATION_QUEUE:
+        if mobile_token in MOBILE_NOTIFICATION_QUEUE:
             try:
-                item = MOBILE_NOTIFICATION_QUEUE[device_token].get(timeout=0)
+                item = MOBILE_NOTIFICATION_QUEUE[mobile_tokenmobile_token].get(timeout=0)
                 return self.create_response(request, item)
             except Queue.Empty:
                 pass
 
-        raise NotFound
+        return self.create_response(request, None, response_class=HttpNotFound)
 
     def add_to_queue(self, user_id, payload):
         user_id = 1
 
-        token = USER_TO_MOBILE_TOKEN_MAP.get(user_id, None)
+        mobile_token = USER_TO_MOBILE_TOKEN_MAP.get(user_id, None)
 
         if not token:
             return
@@ -82,4 +83,4 @@ class MobileResource(Resource):
         if token not in MOBILE_NOTIFICATION_QUEUE:
             MOBILE_NOTIFICATION_QUEUE[token] = Queue.Queue()
 
-        MOBILE_NOTIFICATION_QUEUE[token].put({'token': token, 'payload': payload})
+        MOBILE_NOTIFICATION_QUEUE[token].put({'mobile_token': token, 'payload': payload})
