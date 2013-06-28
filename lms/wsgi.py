c3cd922ea0d36b8a60e9911baccbace3855d286f
Adam Palay <adam@edx.org>
2013-06-28 18:39:31 -0400

    change wsgi handler to force django to read full request before responding

--- lms/wsgi.py
+++ lms/wsgi.py
@@ -4,8 +4,21 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lms.envs.aws")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.core.wsgi import WSGIHandler
+
+
+class ForceDataReadHandler(WSGIHandler):
+    def get_response(self, request):
+        """
+        GET and POST are lazy evaluated.
+        Force read them before passing them to django
+        """
+        query = request.GET
+        data = request.POST
+
+        return super(ForceDataReadHandler, self).get_response(request)
+
+application = ForceDataReadHandler()
 
 from django.conf import settings
 from xmodule.modulestore.django import modulestore
