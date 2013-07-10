7a52e6f947f5df0394f7f32196fd14e5852aea4b
John Hess <mgojohn@gmail.com>
2013-07-10 13:51:31 -0400

    move secondary_db decorator to util and use simpler settings

--- lms/djangoapps/dashboard/views.py
+++ lms/djangoapps/dashboard/views.py
@@ -1,6 +1,5 @@
 import json
 import calendar
-import functools
 import time
 from datetime import date, datetime, timedelta
 from operator import itemgetter
@@ -8,49 +7,12 @@ from operator import itemgetter
 from django.http import Http404, HttpResponse
 from django.db import connections
 from django.conf import settings
-from mitxmako.shortcuts import render_to_response
 
+from mitxmako.shortcuts import render_to_response
 from django.contrib.auth.models import User
 from student.models import CourseEnrollment, CourseEnrollmentAllowed
 from certificates.models import GeneratedCertificate
-
-class prefer_secondary_db(object):
-    """
-    Decorator. 
-
-    If an "replica" db is specified in django settings, 
-    it is provided to the function as the the value of the
-    argument `secondary_db`.
-
-    Example:
-
-    @prefer_secondary_db('replica')
-    def count_users(secondary_db=None):
-        assert secondary_db is not None
-        return User.objects.using(secondary_db).filter().count()
-
-    count_users()
-    
-    will provide different results depending on the dbs defined in
-    settings.py.  If a database named 'replica' is defined, it will
-    return the number of users in the replica database.  If no such
-    db is specified, it will return the number of users in the default
-    db.
-
-    """
-    def __init__(self, secondary_db):
-        if secondary_db in settings.DATABASES:
-            self.secondary_db = secondary_db
-        else:
-            self.secondary_db = 'default'
-
-    def __call__(self, fn):
-        @functools.wraps(fn)
-        def decorated(*args, **kwargs):
-            kwargs['secondary_db'] = self.secondary_db
-            return fn(*args, **kwargs)
-
-        return decorated
+from util.databases import prefer_secondary_db
 
 def parse_course_id(course_id):
     """
