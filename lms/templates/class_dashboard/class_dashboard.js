4a43afb9479b78cb03ad9e9ea81635e1f2ae3340
Kristin Stephens <ksteph@cs.berkeley.edu>
2013-08-02 17:32:19 -0700

    wip: adding files from sandbox

---
+++ lms/templates/class_dashboard/class_dashboard.js
@@ -0,0 +1,20 @@
+<%page args="sandbox_stuff, **kwargs"/>
+<%!
+  import json
+%>
+
+$(function () {
+  var d3_prob_grade_distrib = ${json.dumps(sandbox_stuff['d3_prob_grade_distrib'])}
+  console.log(d3_prob_grade_distrib);
+
+  var svg = d3.select("#viz").append("svg");
+  var div = d3.select("#viz").append("div");
+  var param = {
+    data: d3_prob_grade_distrib,
+    width: 1000,
+    height: 800,
+    bVerticalXAxisLabel: true,
+  };
+  var barGraph = edx_d3CreateStackedBarGraph(param, svg, div);
+  barGraph.drawGraph();
+});
