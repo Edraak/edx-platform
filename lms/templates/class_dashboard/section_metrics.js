f835e7f9db2aba0b8fa89c78866882c81a57b411
Kristin Stephens <ksteph@cs.berkeley.edu>
2013-08-07 15:17:44 -0700

    wip: more style tweaks

--- lms/templates/class_dashboard/section_metrics.js
+++ lms/templates/class_dashboard/section_metrics.js
@@ -9,7 +9,7 @@ $(function () {
   var paramOpened = {
     data: ${json.dumps(data_opened)},
     width: $("#${id_str_opened}").width(),
-    height: $("#${id_str_opened}").height(),
+    height: $("#${id_str_opened}").height()-25, // Account for header
     tag: "opened",
     bVerticalXAxisLabel : true,
     bLegend : false,
@@ -19,14 +19,14 @@ $(function () {
   var paramGrade = {
     data: ${json.dumps(data_grade)},
     width: $("#${id_str_grade}").width(),
-    height: $("#${id_str_grade}").height(),
+    height: $("#${id_str_grade}").height()-25, // Account for header
     tag: "grade",
   };
 
   var paramAttempts = {
     data: ${json.dumps(data_attempts)},
     width: $("#${id_str_attempts}").width(),
-    height: $("#${id_str_attempts}").height(),
+    height: $("#${id_str_attempts}").height()-25, // Account for header
     tag: "attempts",
   };
 
@@ -40,7 +40,10 @@ $(function () {
 
   if ( paramGrade.data.length > 0 ) {
     barGraphGrade = edx_d3CreateStackedBarGraph(paramGrade, d3.select("#${id_str_grade}").append("svg"), divTooltip);
-    barGraphGrade.scale.stackColor.domain([0,50,100]).range(["#e13f29","#dddddd","#17a74d"]);
+
+    barGraphGrade.scale.stackColor.domain([0,50,100]).range(["#e13f29","#cccccc","#17a74d"]);
+    barGraphGrade.legend.width += 2;
+
     barGraphGrade.drawGraph();
   }
 
@@ -48,8 +51,10 @@ $(function () {
     barGraphAttempts = edx_d3CreateStackedBarGraph(paramAttempts, d3.select("#${id_str_attempts}").append("svg"),
                                                    divTooltip);
     barGraphAttempts.scale.stackColor
-      .range(["#b3b9fd","#b1b7fd","#a1a7fd","#8e96fe","#7c85fe","#6a75fe",
-              "#5a66fe","#4a57fe","#3946fd","#2635fe","#1425fe"]);
+      .range(["#c3c4cd","#b0b4d1","#9ca3d6","#8993da","#7682de","#6372e3",
+              "#4f61e7","#3c50eb","#2940ef","#1530f4","#021ff8"]);
+    barGraphAttempts.legend.width += 2;
+
     barGraphAttempts.drawGraph();
   }
 });
