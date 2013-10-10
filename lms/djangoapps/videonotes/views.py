e597865263816c3ce19439b086e056a5f8cc1ccb
Adam Palay <adam@edx.org>
2013-10-10 17:56:09 -0400

    start views

--- lms/djangoapps/videonotes/views.py
+++ lms/djangoapps/videonotes/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+
+from videonotes.models import VideoNote
+
+def add_note(request):
+	timestamp = request.POST.get('timestamp')
+	user = request.POST.get('user')
+	course_id = request.POST.get('course_id')
+
+	VideoNote.objects.create(
+		user=user, timestamp=timestamp, course_id=course_id
+	)
+
+	
