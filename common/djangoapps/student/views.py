8538d0f25bf6fcdcb0d7661766e7bae65be5a7b0
Joe Blaylock <jrbl@jrbl.org>
2013-10-28 18:44:42 -0700

    Dashboard refactoring for rolling certificates
    * Dashboard view refactored to prevent database accesses for
      non-validated user accounts.
    * Studio advanced setting added, "certificates_show_before_end", which
      determines whether a course should permit certificates to be
      downloadable by students before the course's end date has passed.
    * Modifications to dashboard view and templates to allow display of
      certificate download links before courses have ended, while being
      careful not to declare failing students as failing before the course
      has ended.
    * To test, turn the setting on in a course which hasn't ended yet, and force
      certificate generation for a student, then check their dashboard.
    * TODO:
      - needs tests
      - needs pep8/pylint

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -24,11 +24,10 @@ from django.core.urlresolvers import reverse
 from django.core.validators import validate_email, validate_slug, ValidationError
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError, transaction
-from django.http import (HttpResponse, HttpResponseBadRequest, HttpResponseForbidden,
-                         HttpResponseNotAllowed, Http404)
+from django.http import (HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, Http404)
 from django.shortcuts import redirect
 from django_future.csrf import ensure_csrf_cookie
-from django.utils.http import cookie_date, base36_to_int, urlencode
+from django.utils.http import cookie_date, base36_to_int
 from django.utils.translation import ugettext as _
 from django.views.decorators.http import require_POST, require_GET
 from django.contrib.admin.views.decorators import staff_member_required
@@ -184,7 +183,7 @@ def cert_info(user, course):
     'survey_url': url, only if show_survey_button is True
     'grade': if status is not 'processing'
     """
-    if not course.has_ended():
+    if not course.may_certify():
         return {}
 
     return _cert_info(user, course, certificate_status_for_student(user, course.id))
@@ -312,46 +311,57 @@ def complete_course_mode_info(course_id, enrollment):
 @ensure_csrf_cookie
 def dashboard(request):
     user = request.user
-
-    # Build our courses list for the user, but ignore any courses that no longer
-    # exist (because the course IDs have changed). Still, we don't delete those
-    # enrollments, because it could have been a data push snafu.
     courses = []
-    for enrollment in CourseEnrollment.enrollments_for_user(user):
-        try:
-            courses.append((course_from_id(enrollment.course_id), enrollment))
-        except ItemNotFoundError:
-            log.error("User {0} enrolled in non-existent course {1}"
-                      .format(user.username, enrollment.course_id))
-
-    course_optouts = Optout.objects.filter(user=user).values_list('course_id', flat=True)
-
     message = ""
-    if not user.is_active:
-        message = render_to_string('registration/activate_account_notice.html', {'email': user.email})
-
-    # Global staff can see what courses errored on their dashboard
     staff_access = False
     errored_courses = {}
-    if has_access(user, 'global', 'staff'):
-        # Show any courses that errored on load
-        staff_access = True
-        errored_courses = modulestore().get_errored_courses()
+    course_optouts = []
+    show_courseware_links_for = set([])
+    show_email_settings_for = set([])
+    cert_statuses = {}
+    exam_registrations = {}
 
-    show_courseware_links_for = frozenset(course.id for course, _enrollment in courses
-                                          if has_access(request.user, course, 'load'))
+    if user.is_active:
+        # Build our courses list for the user, but ignore any courses that no longer
+        # exist (because the course IDs have changed). Still, we don't delete those
+        # enrollments, because it could have been a data push snafu.
+        for enrollment in CourseEnrollment.enrollments_for_user(user):
+            try:
+                courses.append((course_from_id(enrollment.course_id), enrollment))
+            except ItemNotFoundError:
+                log.error("User {0} enrolled in non-existent course {1}"
+                          .format(user.username, enrollment.course_id))
 
-    course_modes = {course.id: complete_course_mode_info(course.id, enrollment) for course, enrollment in courses}
-    cert_statuses = {course.id: cert_info(request.user, course) for course, _enrollment in courses}
+        course_optouts = Optout.objects.filter(user=user).values_list('course_id', flat=True)
 
-    # only show email settings for Mongo course and when bulk email is turned on
-    show_email_settings_for = frozenset(
-        course.id for course, _enrollment in courses if (
-            settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL'] and
-            modulestore().get_modulestore_type(course.id) == MONGO_MODULESTORE_TYPE and
-            CourseAuthorization.instructor_email_enabled(course.id)
+        # Global staff can see what courses errored on their dashboard
+        if has_access(user, 'global', 'staff'):
+            # Show any courses that errored on load
+            staff_access = True
+            errored_courses = modulestore().get_errored_courses()
+
+        course_modes = {course.id: complete_course_mode_info(course.id, enrollment) for course, enrollment in courses}
+        cert_statuses = {course.id: cert_info(request.user, course) for course, _enrollment in courses}
+
+        # only show email settings for Mongo course and when bulk email is turned on
+        show_email_settings_for = frozenset(
+            course.id for course, _enrollment in courses if (
+                settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL'] and
+                modulestore().get_modulestore_type(course.id) == MONGO_MODULESTORE_TYPE and
+                CourseAuthorization.instructor_email_enabled(course.id)
+            )
         )
-    )
+
+        show_courseware_links_for = frozenset(course.id for course, _enrollment in courses
+                                              if has_access(request.user, course, 'load'))
+
+        cert_statuses = {course.id: cert_info(request.user, course) for course, _enrollment in courses}
+
+        exam_registrations = {course.id: exam_registration_info(request.user, course) for course, _enrollment in courses}
+
+    else:
+        message = render_to_string('registration/activate_account_notice.html', {'email': user.email})
+
     # get info w.r.t ExternalAuthMap
     external_auth_map = None
     try:
@@ -360,16 +370,17 @@ def dashboard(request):
         pass
 
     context = {'courses': courses,
-               'course_optouts': course_optouts,
                'message': message,
-               'external_auth_map': external_auth_map,
                'staff_access': staff_access,
                'errored_courses': errored_courses,
+               'course_optouts': course_optouts,
                'show_courseware_links_for': show_courseware_links_for,
+               'show_email_settings_for': show_email_settings_for,
                'all_course_modes': course_modes,
                'cert_statuses': cert_statuses,
-               'show_email_settings_for': show_email_settings_for,
-               }
+               'exam_registrations': exam_registrations,
+               'external_auth_map': external_auth_map,
+              }
 
     return render_to_response('dashboard.html', context)
 
