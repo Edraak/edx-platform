ece123afb1fece5861503d9bb5579667f305bc00
David Baumgold <david@davidbaumgold.com>
2013-11-22 09:23:34 -0500

    WIP: snapshot API

--- common/djangoapps/edxapi/urls.py
+++ common/djangoapps/edxapi/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('edxapi.views',  # nopep8
     url(r'^indexes/?$', 'list_indexes', name='list_indexes'),
-    url(r'^indexes/(?P<index_id>.*)/?$', 'detail_index', name='detail_index'),
+    url(r'^indexes/(?P<index_id>[\w.]+)/?$', 'detail_index', name='detail_index'),
+    url(r'^indexes/(?P<index_id>[\w.]+)/branches?$', 'index_branches', name='index_branches'),
+    url(r'^indexes/(?P<index_id>[\w.]+)/branches/(?P<branch>\w+)?$', 'index_branch_redirect', name='index_branch_redirect'),
+    url(r'^snapshots/(?P<snapshot_id>[\w.]+)/?$', 'detail_snapshot', name='detail_snapshot'),
+    url(r'^snapshots/(?P<snapshot_id>[\w.]+)/xblocks?$', 'list_xblocks_in_snapshot', name='list_xblocks_in_snapshot'),
+    url(r'^snapshots/(?P<snapshot_id>[\w.]+)/xblocks/(?P<xblock_name>[\w_]+)?$', 'detail_xblock_in_snapshot', name='detail_xblock_in_snapshot'),
 )
--- common/djangoapps/edxapi/views.py
+++ common/djangoapps/edxapi/views.py
@@ -1,6 +1,9 @@
 from xmodule.modulestore.django import modulestore, loc_mapper
+from xmodule.modulestore import Location
 from xmodule.modulestore.locator import CourseLocator
+from django.http import HttpResponseRedirect
 from util.json_request import JsonResponse
+from django.core.urlresolvers import reverse
 
 
 def map_all_courses():
@@ -24,15 +27,47 @@ def should_map_courses(course):
 
 def serialize_course(course):
     m = loc_mapper()
-    published_locator = m.translate_location(course.location.course_id, course.location, True)
-    draft_locator = m.translate_location(course.location.course_id, course.location, False)
+    published_locator = CourseLocator(
+        m.translate_location(course.location.course_id, course.location, True)
+    )
+    draft_locator = CourseLocator(
+        m.translate_location(course.location.course_id, course.location, False)
+    )
     return {
         "id": published_locator.course_id,
         "branches": {
-            "published": unicode(published_locator),
-            "draft": unicode(draft_locator),
+            "published": published_locator.course_id
+            # "draft": unicode(draft_locator),
         }
     }
+
+
+def serialize_snapshot(course):
+    m = loc_mapper()
+    locator = CourseLocator(
+        m.translate_location(course.location.course_id, course.location)
+    )
+    children_locations = [Location(url) for url in course.children]
+    children_locators = [m.translate_location(course.location.course_id, l)
+                         for l in children_locations]
+    return {
+        "id": locator.course_id,
+        "xblocks": [l.usage_id for l in children_locators]
+    }
+
+
+def serialize_xblock(xblock):
+    fields = {key: value.display_name for key, value in xblock.fields.items()}
+    ret =  {
+        "name": xblock.location.name,
+        "type": xblock.module_class.js_module_name,
+        "display": {
+            "name": xblock.display_name
+        },
+        "fields": fields
+    }
+    return ret
+
 
 
 def list_indexes(request):
@@ -57,3 +92,76 @@ def detail_index(request, index_id):
 
     course = modulestore().get_item(location)
     return JsonResponse(serialize_course(course))
+
+
+def index_branches(request, index_id):
+    locator = CourseLocator(course_id=index_id)
+    location = loc_mapper().translate_locator_to_location(
+        locator, get_course=True,
+    )
+    if not location:
+        # maybe it just isn't mapped to a locator?
+        map_all_courses()
+        location = loc_mapper().translate_locator_to_location(
+            locator, get_course=True,
+        )
+    if not location:
+        # doesn't exist
+        return JsonResponse(status=404)
+
+    course = modulestore().get_item(location)
+    return JsonResponse(serialize_course(course)["branches"])
+
+
+def index_branch_redirect(request, index_id, branch="published"):
+    # for now, we only support the "published" branch
+    return HttpResponseRedirect(reverse("detail_snapshot", kwargs={"snapshot_id": index_id}))
+
+
+def detail_snapshot(request, snapshot_id):
+    # before split mongo, we don't have real, immutable snapshots -- so instead
+    # of an ID, we'll use a locator string to indentify the snapshot. :(
+    locator = CourseLocator(course_id=snapshot_id)
+    if locator.branch == "draft":
+        return JsonResponse({
+            "error": "This API cannot display snapshots for draft courses at this time"
+        }, status=501)
+    location = loc_mapper().translate_locator_to_location(locator, get_course=True)
+    if not location:
+        # maybe it just isn't mapped to a locator?
+        map_all_courses()
+        location = loc_mapper().translate_locator_to_location(locator, get_course=True)
+    if not location:
+        # doesn't exist
+        return JsonResponse(status=404)
+
+    course = modulestore().get_item(location)
+    return JsonResponse(serialize_snapshot(course))
+
+
+def list_xblocks_in_snapshot(request, snapshot_id):
+    # before split mongo, we don't have real, immutable snapshots -- so instead
+    # of an ID, we'll use a locator string to indentify the snapshot. :(
+    locator = CourseLocator(course_id=snapshot_id)
+    if locator.branch == "draft":
+        return JsonResponse({
+            "error": "This API cannot display snapshots for draft courses at this time"
+        }, status=501)
+    location = loc_mapper().translate_locator_to_location(locator, get_course=True)
+    if not location:
+        # maybe it just isn't mapped to a locator?
+        map_all_courses()
+        location = loc_mapper().translate_locator_to_location(locator, get_course=True)
+    if not location:
+        # doesn't exist
+        return JsonResponse(status=404)
+
+    course = modulestore().get_item(location)
+    children = [modulestore().get_item(Location(url)) for url in course.children]
+    blocks = [serialize_xblock(child) for child in children]
+    return JsonResponse(blocks)
+
+def detail_xblock_in_snapshot(request, snapshot_id, xblock_name):
+    # TODO
+
+
