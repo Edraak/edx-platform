779fa64c41d321d415849f0c076daace86ffc07b
Oleg Marshev <oleh.marshev@gmail.com>
2014-04-11 15:50:40 +0300

    Work in progress.

--- pavelib/stubs.py
+++ pavelib/stubs.py
@@ -7,6 +7,8 @@ from paver.easy import *
 from .utils.cmd import cmd
 from .utils.process import run_process
 
+from path import path
+
 DEFAULT_PORT = {"lti": 8765, "youtube": 8031}
 
 def run_stub(stub, port=None):
@@ -16,13 +18,15 @@ def run_stub(stub, port=None):
     `settings` is the Django settings module to use; if not provided, use the default.
     `port` is the port to run the server on; if not provided, use the default port for the system.
     """
+    sys.path.append('/edx/app/edxapp/edx-platform/common/djangoapps/terrain/stubs')
+
     if stub not in ['lti', 'youtube']:
         print("Stub must be either lti or youtube", file=sys.stderr)
         exit(1)
 
     if port is None:
         port = DEFAULT_PORT[stub]
-    run_process(cmd('python', '-m', 'stubs.start', stub, port))
+    run_process(cmd('python', '-m', 'start', stub, port))
 
 @task
 @needs('pavelib.prereqs.install_prereqs')
