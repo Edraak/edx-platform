fee72f415f76b2252b03332c858af0c2e7ecb513
Ben McMorran <ben.mcmorran@gmail.com>
2014-06-12 20:23:01 -0400

    Adds crossword generation and enables advanced components.

--- xblock/crossword/crossword/crossword.py
+++ xblock/crossword/crossword/crossword.py
@@ -32,6 +32,32 @@ class CrosswordXBlock(XBlock):
         The primary view of the CrosswordXBlock, shown to students
         when viewing courses.
         """
+
+        word_list = ['saffron', 'The dried, orange yellow plant used to as dye and as a cooking spice.'], \
+            ['pumpernickel', 'Dark, sour bread made from coarse ground rye.'], \
+            ['leaven', 'An agent, such as yeast, that cause batter or dough to rise..'], \
+            ['coda', 'Musical conclusion of a movement or composition.'], \
+            ['paladin', 'A heroic champion or paragon of chivalry.'], \
+            ['syncopation', 'Shifting the emphasis of a beat to the normally weak beat.'], \
+            ['albatross', 'A large bird of the ocean having a hooked beek and long, narrow wings.'], \
+            ['harp', 'Musical instrument with 46 or more open strings played by plucking.'], \
+            ['piston', 'A solid cylinder or disk that fits snugly in a larger cylinder and moves under pressure as in an engine.'], \
+            ['caramel', 'A smooth chery candy made from suger, butter, cream or milk with flavoring.'], \
+            ['coral', 'A rock-like deposit of organism skeletons that make up reefs.'], \
+            ['dawn', 'The time of each morning at which daylight begins.'], \
+            ['pitch', 'A resin derived from the sap of various pine trees.'], \
+            ['fjord', 'A long, narrow, deep inlet of the sea between steep slopes.'], \
+            ['lip', 'Either of two fleshy folds surrounding the mouth.'], \
+            ['lime', 'The egg-shaped citrus fruit having a green coloring and acidic juice.'], \
+            ['mist', 'A mass of fine water droplets in the air near or in contact with the ground.'], \
+            ['plague', 'A widespread affliction or calamity.'], \
+            ['yarn', 'A strand of twisted threads or a long elaborate narrative.'], \
+            ['snicker', 'A snide, slightly stifled laugh.']
+
+        a = Crossword(13, 13, '-', 5000, word_list)
+        a.compute_crossword(2)
+        self.crossword = a.solution()
+
         html = self.resource_string("static/html/crossword.html")
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/crossword.css"))
