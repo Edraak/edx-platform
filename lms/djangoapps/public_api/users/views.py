16b5fb88c222e0ccc343ba5d457247ae32f70048
David Ormsbee <dave@edx.org>
2014-06-30 17:14:30 -0400

    Protect user parts of the mobile API.

--- lms/djangoapps/public_api/users/views.py
+++ lms/djangoapps/public_api/users/views.py
@@ -1,17 +1,27 @@
-from rest_framework import generics
+from django.core.exceptions import PermissionDenied
+
+from rest_framework import generics, permissions
+from rest_framework.authentication import OAuth2Authentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from student.models import CourseEnrollment, User
 
 from .serializers import CourseEnrollmentSerializer, UserSerializer
+
+
+class IsUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj
+
 
 class UserDetail(generics.RetrieveAPIView):
     """Read-only information about our User.
 
     This will be where users are redirected to after API login and will serve
     as a place to list all useful resources this user can access.
-
-    TODO: Need to add permissions mixin + token auth
     """
+    authentication_classes = (OAuth2Authentication, SessionAuthentication)
+    permission_classes = (permissions.IsAuthenticated, IsUser)
     queryset = (
         User.objects.all()
         .select_related('profile', 'course_enrollments')
@@ -21,16 +31,19 @@ class UserDetail(generics.RetrieveAPIView):
 
 
 class UserCourseEnrollmentsList(generics.ListAPIView):
-    """Read-only list of courses that this user is enrolled in.
-
-    TODO: Need to add permissions mixin + token auth
-    """
-    queryset = (
-        CourseEnrollment.objects.all()
-    )
+    """Read-only list of courses that this user is enrolled in."""
+    authentication_classes = (OAuth2Authentication, SessionAuthentication)
+    permission_classes = (permissions.IsAuthenticated, IsUser)
+    queryset = CourseEnrollment.objects.all()
     serializer_class = CourseEnrollmentSerializer
     lookup_field = 'username'
 
     def get_queryset(self):
         user = User.objects.get(username=self.kwargs['username'])
         return self.queryset.filter(user=user).order_by('created')
+
+    def get(self, request, *args, **kwargs):
+        if request.user.username != kwargs['username']:
+            raise PermissionDenied
+
+        return super(UserCourseEnrollmentsList, self).get(self, request, *args, **kwargs)
