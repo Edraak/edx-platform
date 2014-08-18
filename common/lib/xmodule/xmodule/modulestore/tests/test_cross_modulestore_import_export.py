8d16fff880b7dbfcaba37652a1070350e3aa8a50
David Baumgold <david@davidbaumgold.com>
2014-08-18 11:59:11 -0400

    Skip slow tests

--- common/lib/xmodule/xmodule/modulestore/tests/test_cross_modulestore_import_export.py
+++ common/lib/xmodule/xmodule/modulestore/tests/test_cross_modulestore_import_export.py
@@ -17,6 +17,7 @@ import random
 from contextlib import contextmanager, nested
 from shutil import rmtree
 from tempfile import mkdtemp
+from unittest import skip
 
 from xmodule.tests import CourseComparisonTest
 
@@ -236,6 +237,7 @@ COURSE_DATA_NAMES = (
 )
 
 
+@skip("These tests are too slow to run regularly (over 10 seconds each)")
 @ddt.ddt
 class CrossStoreXMLRoundtrip(CourseComparisonTest):
     """
