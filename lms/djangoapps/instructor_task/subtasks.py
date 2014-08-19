d61101ee1ee3fba8075705046b4e870bf0757f12
Adam Palay <adam@edx.org>
2014-08-19 17:23:23 -0400

    use increment instead of histogram

--- lms/djangoapps/instructor_task/subtasks.py
+++ lms/djangoapps/instructor_task/subtasks.py
@@ -61,7 +61,7 @@ def track_memory_usage(metric):
     for memory_type, baseline_usage in zip(memory_types, baseline_usages):
         total_usage = getattr(process.get_memory_info(), memory_type)
         memory_used = total_usage - baseline_usage
-        dog_stats_api.histogram(metric + "." + memory_type, memory_used)
+        dog_stats_api.increment(metric + "." + memory_type, memory_used)
 
 
 def _generate_items_for_subtask(item_queryset, item_fields, total_num_items, items_per_task, total_num_subtasks):
