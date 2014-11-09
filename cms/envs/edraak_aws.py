f3499e5f58ff16a31afca18c2b9fbe1d094ea7ed
Omar Al-Ithawi <i@omardo.com>
2014-11-09 16:49:10 +0200

    Add CMS_BASE to aws envs

--- cms/envs/edraak_aws.py
+++ cms/envs/edraak_aws.py
@@ -2,6 +2,7 @@ from .aws import *
 from .edraak_common import *
 from lms.envs.edraak_aws import (
     LMS_BASE,
+    CMS_BASE,
     SESSION_COOKIE_DOMAIN,
     STATIC_ROOT_BASE,
     STATIC_URL_BASE,
