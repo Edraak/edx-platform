6df57c1fafe2197ce04abea4e5bef8561a2118c2
Omar Al-Ithawi <i@omardo.com>
2015-02-10 11:06:14 +0200

    Make Edraak cert working for course re-runs

--- lms/djangoapps/edraak_certificates/urls.py
+++ lms/djangoapps/edraak_certificates/urls.py
@@ -1,13 +1,12 @@
 from django.conf import settings
 from django.conf.urls import patterns, url
-from django.core.urlresolvers import LocaleRegexURLResolver
 
 urlpatterns = patterns('',  # nopep8
                        url(r'^issue$', 'edraak_certificates.views.issue', name='edraak_certificates_issue'),
 
-                       url(r'^download/(?P<course_id>[^/]+/[^/]+/[^/]+)$', 'edraak_certificates.views.download',
+                       url(r'^download/{}$'.format(settings.COURSE_ID_PATTERN), 'edraak_certificates.views.download',
                            name='edraak_certificates_download'),
 
-                       url(r'^preview/(?P<course_id>[^/]+/[^/]+/[^/]+)$', 'edraak_certificates.views.preview',
+                       url(r'^preview/{}$'.format(settings.COURSE_ID_PATTERN), 'edraak_certificates.views.preview',
                            name='edraak_certificates_preview'),
 )
