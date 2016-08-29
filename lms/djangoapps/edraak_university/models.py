74d6f60ec79a8929be43af1667bf7cbe8ccc0057
Omar Al-Ithawi <i@omardo.com>
2016-08-29 17:02:19 +0300

    Edraak University ID: Instructor management view

--- lms/djangoapps/edraak_university/models.py
+++ lms/djangoapps/edraak_university/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from student.models import UserProfile
+
 from xmodule_django.models import CourseKeyField
 
 
@@ -12,12 +14,47 @@ class UniversityID(models.Model):
     section_number = models.CharField(max_length=10)
     date_created = models.DateTimeField(default=timezone.now)
 
+    # Will be used in `get_marked_university_ids()` method to mark
+    # duplicate entries.
+    is_conflicted = False
+
+    def get_full_name(self):
+        try:
+            user_profile = UserProfile.objects.get(user=self.user)
+            return user_profile.name
+        except UserProfile.DoesNotExist:
+            return None
+
+    def get_email(self):
+        return self.user.email
+
     def __unicode__(self):
         return u'{user} - {course_key} - {university_id}'.format(
             user=self.user,
             course_key=self.course_key,
             university_id=self.university_id,
         )
+
+    @classmethod
+    def get_marked_university_ids(cls, course_key):
+        queryset = cls.objects.filter(course_key=course_key)
+        queryset = queryset.order_by('university_id')
+
+        def cleanup_id(id):
+            """
+            Trim an ID to make it easier to compare.
+            """
+            return id.strip().lower()
+
+        entries = list(queryset)
+        for i, entry in enumerate(entries):
+            if i > 0:  # Avoid IndexError
+                prev_entry = entries[i-1]
+                if cleanup_id(entry.university_id) == cleanup_id(prev_entry.university_id):
+                    entry.is_conflicted = True
+                    prev_entry.is_conflicted = True
+
+        return entries
 
     class Meta:
         unique_together = ('user', 'course_key',)
--- lms/djangoapps/edraak_university/urls.py
+++ lms/djangoapps/edraak_university/urls.py
@@ -4,7 +4,8 @@ URLs for the Edraak University ID app.
 from django.conf.urls import patterns, url
 from django.conf import settings
 
-from views import UniversityIDSuccessView
+import views
+
 
 
 urlpatterns = patterns('',  # nopep8
@@ -15,7 +16,22 @@ urlpatterns = patterns('',  # nopep8
     ),
     url(
        r'^id/{}/success$'.format(settings.COURSE_ID_PATTERN),
-       UniversityIDSuccessView.as_view(),
+       views.UniversityIDSuccessView.as_view(),
        name='edraak_university_id_success',
+    ),
+    url(
+        r'^id/{}/instructor/list$'.format(settings.COURSE_ID_PATTERN),
+        views.UniversityIDListView.as_view(),
+        name='edraak_university_id_list',
+    ),
+    url(
+        r'^id/{}/instructor/update/(?P<pk>\d+)$'.format(settings.COURSE_ID_PATTERN),
+        views.UniversityIDUpdateView.as_view(),
+        name='edraak_university_id_update',
+    ),
+    url(
+        r'^id/{}/instructor/delete/(?P<pk>\d+)$'.format(settings.COURSE_ID_PATTERN),
+        views.UniversityIDDeleteView.as_view(),
+        name='edraak_university_id_delete',
     ),
 )
