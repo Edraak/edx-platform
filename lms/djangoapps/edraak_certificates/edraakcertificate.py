9c941e07efab0e02189be29fd82f289c656582f4
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-02-07 10:38:48 +0200

    Implement hack to solve CM mistake

--- lms/djangoapps/edraak_certificates/edraakcertificate.py
+++ lms/djangoapps/edraak_certificates/edraakcertificate.py
@@ -28,7 +28,7 @@ for font_file, font_name in fonts.iteritems():
 SIZE = landscape(A4)
 
 
-def get_organization_logo(organization):
+def get_organization_logo(organization, course_id):
     organization = organization.lower()
     if organization == 'mitx' or organization == 'harvardx' or organization == 'qrf':
         return 'edx.png'
@@ -58,6 +58,8 @@ def get_organization_logo(organization):
         return 'mbrcgi.png'
     elif organization == 'psut':
         return 'psut.png'
+    elif course_id == 'course-v1:Edraak+STEAM101+R1_Q1_2017':
+        return 'auc.jpg'
     else:
         return None
 
@@ -220,9 +222,9 @@ class EdraakCertificate(object):
                 self.ctx.drawString(self.bidi_x_axis(x), y, line)
                 y -= line_height
 
-    def add_course_org_logo(self, course_org):
+    def add_course_org_logo(self, course_org, course_id):
         if course_org:
-            logo = get_organization_logo(course_org)
+            logo = get_organization_logo(course_org, course_id)
             if logo:
                 image = utils.ImageReader(path.join(static_dir, logo))
 
@@ -242,8 +244,8 @@ class EdraakCertificate(object):
 
                 self.ctx.drawImage(image, x, y, width, height)
 
-    def add_course_sponsor_logo(self, sponsor):
-        logo = get_organization_logo(sponsor)
+    def add_course_sponsor_logo(self, sponsor, course_id):
+        logo = get_organization_logo(sponsor, course_id)
         if logo:
             image = utils.ImageReader(path.join(static_dir, logo))
 
@@ -305,7 +307,7 @@ class EdraakCertificate(object):
 
         x = 10.8
         self.add_certificate_bg()
-        self.add_course_org_logo(self.course_org)
+        self.add_course_org_logo(self.course_org, self.course_id)
 
         self.draw_bidi_text(self._("This is to certify that:"), x, 5.8, size=0.25)
 
@@ -320,7 +322,7 @@ class EdraakCertificate(object):
         if sponsor:
             self.draw_single_line_bidi_text(self.course_name, x, 4.1, size=course_name_size, bold=True)
             self.draw_bidi_text(self._("This course is sponsored by:"), x, 3.5, size=0.25)
-            self.add_course_sponsor_logo(sponsor)
+            self.add_course_sponsor_logo(sponsor, self.course_id)
         else:
             self.draw_bidi_text(self.course_name, x, 4.1, size=course_name_size, bold=True)
             if not self.is_english_course():
