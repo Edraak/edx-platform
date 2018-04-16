fa96e60244b4cf3f992d2b3e8da1a640e029f03e
Radwan Abu-Odeh <rabuodeh@qrf.org>
2018-04-16 16:09:33 +0300

    Edraak Certificates integration with edx certificates api

--- lms/djangoapps/edraak_certificates/urls.py
+++ lms/djangoapps/edraak_certificates/urls.py
@@ -8,18 +8,13 @@ urlpatterns = patterns('',  # nopep8
                            name='edraak_certificates_issue'
                        ),
                        url(
-                           r'^view/{}$'.format(settings.COURSE_ID_PATTERN),
-                           'edraak_certificates.views.view',
-                           name='edraak_certificates_view'
+                           r'^check/{}$'.format(settings.COURSE_ID_PATTERN),
+                           'edraak_certificates.views.check_status',
+                           name='edraak_certificates_check_status'
                        ),
                        url(
                            r'^download/{}$'.format(settings.COURSE_ID_PATTERN),
                            'edraak_certificates.views.download',
                            name='edraak_certificates_download'
-                       ),
-                       url(
-                           r'^preview/{}$'.format(settings.COURSE_ID_PATTERN),
-                           'edraak_certificates.views.preview',
-                           name='edraak_certificates_preview'
                        ),
 )
