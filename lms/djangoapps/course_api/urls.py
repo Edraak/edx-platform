0af8ffba8b9efe87c1b0d2609395ecd682d9b886
Fahmi <fahmi.najjar@gmail.com>
2019-05-01 10:44:21 +0300

    create specialization courses API

--- lms/djangoapps/course_api/urls.py
+++ lms/djangoapps/course_api/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls import patterns, url, include
 
 from .views import (
-    CourseDetailView, CourseListView, MarketingCourseDetailView
+    CourseDetailView, CourseListView, MarketingCourseDetailView, SpecializationCourseListView
 )
 
 # EDRAAK: if marketing site is enabled use the marketing details
@@ -17,6 +17,7 @@ else:
 urlpatterns = patterns(
     '',
     url(r'^v1/courses/$', CourseListView.as_view(), name="course-list"),
+    url(r'^v1/(?P<specialization_slug>{})/$'.format(settings.SPECIALIZATION_SLUG_RGX), SpecializationCourseListView.as_view(), name="spec-course-list"),
     url(r'^v1/courses/{}'.format(settings.COURSE_KEY_PATTERN), course_detail_view, name="course-detail"),
     url(r'', include('course_api.blocks.urls'))
 )
