c25adead6575531100f835ba17cf4cc6d83dfa42
Salah Alomari <s.z.alomari.1987@gmail.com>
2019-05-29 13:19:26 +0300

    Only allow one-to-one mapping

--- common/djangoapps/edraak_specializations/models.py
+++ common/djangoapps/edraak_specializations/models.py
@@ -5,7 +5,7 @@ class CourseSpecializationInfo(models.Model):
     """
     Model for saving the specialization info of a course
     """
-    course_id = models.CharField(max_length=255, db_index=True)
+    course_id = models.CharField(max_length=255, db_index=True, unique=True)
     specialization_slug = models.CharField(max_length=255)
     name_ar = models.CharField(max_length=255, blank=True)
     name_en = models.CharField(max_length=255, blank=True)
