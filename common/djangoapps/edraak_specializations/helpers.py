47ac5d812d160a9bd25787fb47ee18d5eefe9cc2
Salah Alomari <s.z.alomari.1987@gmail.com>
2019-06-20 17:11:43 +0300

    Change to specialization PROGS link

--- common/djangoapps/edraak_specializations/helpers.py
+++ common/djangoapps/edraak_specializations/helpers.py
@@ -32,9 +32,9 @@ def get_specialization_info(course_id):
     info["title"] = info_obj.name_en if get_language() == "en"\
         else info_obj.name_ar
     info["link"] = urljoin(
-        settings.MKTG_URLS.get('ROOT'),
-        settings.MKTG_URLS.get("SPECIALIZATION_INFO", "").format(
-            slug=info_obj.specialization_slug
+        settings.PROGS_URLS.get('ROOT'),
+        settings.PROGS_URLS.get("SPECIALIZATION_LMS", "").format(
+            specialization_slug=info_obj.specialization_slug
         )
     )
     return info
